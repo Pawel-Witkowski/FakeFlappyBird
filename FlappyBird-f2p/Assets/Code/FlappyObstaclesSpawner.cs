6e7437c472116bbe1b9f3a428c1bc13623b2856a
Pawel-Witkowski <witkowski.pawel@hotmail.com>
2017-09-26 13:08:44 +0200

    Added destroyer and colliders

---
+++ FlappyBird-f2p/Assets/Code/FlappyObstaclesSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FlappyObstaclesSpawner : MonoBehaviour {
+
+	public GameObject obstaclePrefab;
+    public Transform camera;
+
+	List<GameObject> spawnedObstacles = new List<GameObject>();
+
+	void Spawn( float x, float y, float gapHeight ) {
+		GameObject spawned = GameObject.Instantiate( obstaclePrefab );
+        spawned.transform.parent = transform;
+		spawned.transform.position = new Vector3( x, y, 0 );
+		spawnedObstacles.Add( spawned );
+
+        Transform bottomTransform = spawned.transform.Find( "Bottom" );
+        Transform topTransform = spawned.transform.Find( "Top" );
+        float bottomY = -gapHeight/2;
+        float topY = +gapHeight/2;
+        bottomTransform.localPosition = Vector3.up * bottomY;
+        topTransform.localPosition = Vector3.up * topY;
+    }
+
+}
---
+++ FlappyBird-f2p/Assets/Code/ObstaclesDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesDestroyer : MonoBehaviour {
+
+    public Transform objectToFollow;
+    private Vector3 offset;
+	void Start () {
+        offset = objectToFollow.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = objectToFollow.position - offset;	
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        Destroy(other.gameObject, 1f);
+    }
+}
