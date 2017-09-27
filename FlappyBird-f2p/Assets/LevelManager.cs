c4ca4b029c39ae72a4427e0c9d7d9f7868908ee4
Pawel-Witkowski <witkowski.pawel@hotmail.com>
2017-09-27 16:44:05 +0200

    Menu system
    Created level manager, menu, added particle system - yet not fully
    working

---
+++ FlappyBird-f2p/Assets/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    private void Awake() {
+        LevelManager tempLM = FindObjectOfType<LevelManager>();
+        if (tempLM != null && tempLM != this) {
+            Destroy(tempLM.gameObject);
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);		
+	}
+	
+    public void LoadLevel(string LevelName) {
+        Application.LoadLevel(LevelName);
+    }
+	// Update is called once per frame
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
